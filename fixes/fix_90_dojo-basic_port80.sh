48959f2524da116c111bb59b37040f4bc8b2bda9
Ben Allen <benjamin+samurai@umn.edu>
2013-07-23 21:55:05 -0500

    clean up formatting of backup file name/extension

--- fixes/fix_90_dojo-basic_port80.sh
+++ fixes/fix_90_dojo-basic_port80.sh
@@ -3,5 +3,5 @@
 FILES="./files"
 DATE=$(date +%F_%R:%S | sed 's/://g')
 
-install -S ${DATE} -o samurai -g samurai -m 644 ${FILES}/dojo-basic /etc/apache2/sites-available/
+install -S .${DATE} -o samurai -g samurai -m 644 ${FILES}/dojo-basic /etc/apache2/sites-available/
 service apache2 restart
