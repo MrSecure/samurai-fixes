c1b802a6d6a0c909b514e9a299a53b89f8787213
Ben Allen <benjamin+samurai@umn.edu>
2013-07-23 21:45:12 -0500

    ignore the updates log; fix dojo-basic to process requests on port 80 so that ZAP brute force example works properly

---
+++ fixes/fix_90_dojo-basic_port80.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FILES="./files"
+DATE=$(date +%F_%R:%S | sed 's/://g')
+
+install -S ${DATE} -o samurai -g samurai -m 644 ${FILES}/dojo-basic /etc/apache2/sites-available/
+service apache2 restart
