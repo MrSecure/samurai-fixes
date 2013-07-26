41334ca8fb90104e20668f4f401c6dd94a6827be
Ben Allen <benjamin+samurai@umn.edu>
2013-07-26 12:05:31 -0500

    added script to enable dangerous mode, apache configs for same

---
+++ be_dangerous.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo
+echo "This script puts Samurai WTF's vulnerable targets into world accessible mode"
+echo 
+echo "    ***   THIS IS CONSIDERED DANGEROUS TO THE SECURITY OF YOUR SYSTEM!! ***"
+echo
+echo "Remember to run this script using sudo!"
+echo "Press any key to continue..."
+read KEY
+
+D=""
+FILES="./fixes/files"
+DATE=$(date +%F_%R:%S | sed 's/://g')
+
+SITES="dojo-basic dojo-scavenger dvwa mutillidae"
+for site in $SITES ; do
+  $D install -S .${DATE} -o samurai -g samurai -m 644 ${FILES}/${site}.dangerous /etc/apache2/sites-available/${site}
+done
+
+## install -S .${DATE} -o samurai -g samurai -m 644 ${FILES}/dojo-basic /etc/apache2/sites-available/
+$D service apache2 restart
